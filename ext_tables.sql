23295ae725d5f7c3e80e44b595aeaa4493c07d0c
Michael Perlbach <info@mikelmade.de>
2018-12-17 11:50:16 +0100

    Update ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,7 +3,7 @@
 #
 CREATE TABLE tx_mmimagemap_domain_model_map (
 	uid int(11) NOT NULL auto_increment,
-	pid int(11) NOT NULL DEFAULT '0',
+	pid int(11) NOT NULL DEFAULT '1',
 	name tinytext NOT NULL,
 	imgfile tinytext NOT NULL,
   altfile varchar(255) NOT NULL DEFAULT '',
@@ -17,7 +17,7 @@ CREATE TABLE tx_mmimagemap_domain_model_map (
 #
 CREATE TABLE tx_mmimagemap_domain_model_area (
 	uid int(11) NOT NULL auto_increment,
-	pid int(11) NOT NULL DEFAULT '0',
+	pid int(11) NOT NULL DEFAULT '1',
 	mapid int(11) NOT NULL DEFAULT '0',
 	areatype int(11) NOT NULL DEFAULT '0',
 	arealink tinytext NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE tx_mmimagemap_domain_model_area (
 #
 CREATE TABLE tx_mmimagemap_domain_model_point (
 	uid int(11) NOT NULL auto_increment,
-	pid int(11) NOT NULL DEFAULT '0',
+	pid int(11) NOT NULL DEFAULT '1',
 	areaid int(11) NOT NULL DEFAULT '0',
 	num int(11) NOT NULL DEFAULT '0',
 	x int(11) NOT NULL DEFAULT '0',
@@ -53,7 +53,7 @@ CREATE TABLE tx_mmimagemap_domain_model_point (
 #
 CREATE TABLE tx_mmimagemap_domain_model_bcolors (
    uid int(11) NOT NULL auto_increment,
-	 pid int(11) NOT NULL DEFAULT '0',
+	 pid int(11) NOT NULL DEFAULT '1',
    mapid int(11) NOT NULL DEFAULT '0',
    colorname varchar(255) NOT NULL DEFAULT '',
    color varchar(7) NOT NULL DEFAULT '',
@@ -66,7 +66,7 @@ CREATE TABLE tx_mmimagemap_domain_model_bcolors (
 #
 CREATE TABLE tx_mmimagemap_domain_model_contentpopup (
 	uid int(11) NOT NULL auto_increment,
-	pid int(11) NOT NULL DEFAULT '0',
+	pid int(11) NOT NULL DEFAULT '1',
 	areaid int(11) NOT NULL DEFAULT '0',
   contentid int(11) NOT NULL DEFAULT '0',
   popupwidth int(11) NOT NULL DEFAULT '0',
