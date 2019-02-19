e0feff05a205e65cbb8b6fcc6fe3f7c4fd084d59
Michael Perlbach <info@mikelmade.de>
2019-02-19 06:42:25 +0100

    optimizing ContentViewHelper for TYPO3 9.x

--- Classes/ViewHelpers/ContentViewHelper.php
+++ Classes/ViewHelpers/ContentViewHelper.php
@@ -2,32 +2,38 @@
 namespace MikelMade\Mmimagemap\ViewHelpers;
 
 /**
- * (c) 2018 MMichael Perlbach
+ * (c) 2019 MMichael Perlbach
  */
 
 
 class ContentViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
 {
-    // output html since TYPO3 8LTS
-    protected $escapeOutput = false;
+		// output html since TYPO3 8LTS
+		protected $escapeOutput = false;
 
-    /**
-     * Parse content element
-     *
-     * @param  int     UID des Content Element
-     * @return   string  Geparstes Content Element
-     */
-     
+		/**
+		 * register the content element uid
+		 */
+		public function initializeArguments()
+		{
+			parent::initializeArguments();
+			$this->registerArgument('uid', 'int', 'The uid of the plugin content', true);
+		}
 
-     
-    public function render($uid)
-    {
-        $conf = array(
-      'tables' => 'tt_content',
-      'source' => $uid,
-      'dontCheckPid' => 1
-    );
-        
-        return $this->objectManager->get('TYPO3\CMS\Frontend\ContentObject\RecordsContentObject')->render($conf);
-    }
+		/**
+		 * Parse content element
+		 *
+		 * @return	 string	parsed content element
+		 */
+		public function render()
+		{
+			$uid = (int)$this->arguments['uid']; print $uid;
+			$conf = array(
+				'tables' => 'tt_content',
+				'source' => $uid,
+				'dontCheckPid' => 1
+			);
+				
+			return $this->objectManager->get('TYPO3\CMS\Frontend\ContentObject\RecordsContentObject')->render($conf);
+		}
 }
